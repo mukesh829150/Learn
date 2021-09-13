43601ec572eed6c0c2ecd88518f6cf2b7d516f7e
Gaurav <gklal210@hotmail.com>
2021-09-14 04:05:33 +0530

    New Project

---
+++ Learn/src/javaBasics/AccessModifiers.java
@@ -0,0 +1,31 @@
+package javaBasics;
+
+import interviewPrograms.StringOutput;
+
+public class AccessModifiers extends StringOutput{
+	
+	StaticBlocks a = new StaticBlocks();
+	StringOutput b = new StringOutput();
+	
+	public static void main(String[] args) {
+		AccessModifiers obj = new AccessModifiers(1);
+	}
+	
+	private AccessModifiers() {
+		
+	}
+	
+	private AccessModifiers(int a) {
+		System.out.println(a);
+	}
+	
+	void access(){
+		methodPrtd();
+		mtdDefaultupdatedtpPrtd();
+		a.methodPrtd();
+		a.mtdDefault();
+	}
+	
+
+
+}
